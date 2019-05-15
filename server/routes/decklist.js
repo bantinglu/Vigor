97ad8e1f98feb5ab6321e6a4e8885568f17aa094
Banting Lu <banting.lu.0514@gmail.com>
2019-05-15 00:58:51 -0400

    text to decklist

--- server/routes/decklist.js
+++ server/routes/decklist.js
@@ -2,22 +2,27 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const postAPI = 'https://jsonplaceholder.typicode.com';
+var fs = require("fs");
 //GET
 
 router.get('/', (req, res)=>{
-    posts = [{
-            "Quantity": 4,
-            "Name": "Primeval Titan"
-        },
-        {
-            "Quantity": 4,
-            "Name": "Amulet of Vigor"
-        },
-        {
-            "Quantity": 4,
-            "Name": "Anicent Stirrings"
-    }];
-    res.status(200).json(posts);
+    posts = [];
+
+    fs.readFile('./server/routes/Amulit.txt', function(err, data) {
+        if(err) throw err;
+        var array = data.toString().split("\n");
+        for(i in array) {
+            var quant = array[i].substr(0,array[i].indexOf(' ')); 
+            var name = array[i].substr(array[i].indexOf(' ')+1);
+            var obj = {
+                "Quantity" : quant,
+                "Name" : name
+            }
+            posts.push(obj);
+        }
+
+        res.status(200).json(posts);
+    });
        
 });
 
