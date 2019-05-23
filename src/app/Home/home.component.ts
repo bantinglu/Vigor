ae8af1c95753ac1ce9287f8e6893b1531b7cdc9d
Banting Lu <banting.lu.0514@gmail.com>
2019-05-23 19:26:56 -0400

    mouseover display card

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -9,6 +9,7 @@ import { DecklistService } from './home.service';
 })
 export class HomeComponent {
   posts: any = [];
+  imageSource : any;
 
   constructor(private decklistService: DecklistService) { }
   ngOnInit(){
@@ -18,6 +19,16 @@ export class HomeComponent {
   }
 
   public executeSelectedChange = (event) => {
+    this.imageSource = "assets/cardBack.jpg";
     console.log(event);
+  }
+
+  public displayCard = (name) =>{
+    this.decklistService.getCard(name).subscribe(card=>{
+      this.imageSource = card['image_uris']['large'];
+    });
+  }
+  public click = () =>{
+    console.log("click");
   }
 }
--- src/app/Home/home.service.ts
+++ src/app/Home/home.service.ts
@@ -13,4 +13,12 @@ export class DecklistService {
         return posts;
     });
   }
+
+  getCard(name){
+    console.log("called");
+    return this.http.get('https://api.scryfall.com/cards/named?fuzzy='+name).map((posts)=>{
+      console.log(posts)
+        return posts;
+    });
+  }
 }
