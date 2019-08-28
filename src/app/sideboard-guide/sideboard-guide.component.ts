a842940019a19aef45528aeefa3cd9629aacd1d9
Banting Lu <banting.lu.0514@gmail.com>
2019-08-27 23:06:43 -0400

    started SB stuff

--- src/app/sideboard-guide/sideboard-guide.component.ts
+++ src/app/sideboard-guide/sideboard-guide.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sideboard-guide.component.css']
 })
 export class SideboardGuideComponent implements OnInit {
-
+  test = [{
+    "id": "One",
+    "deck": "humans",
+    "desc": "The Humans Matchup revolves controlling the board and then turning " +
+            "with a titan or a really large walking ballista. You will keep the board clear using engineered explsoives. " +
+            "Be wary of reflector mage on titan or them copying the titan with a phantasmal image. Phantasmal Image can be killed with your slayer's stronghold. " +
+            "Board in anything that can control the board (EE, path, Hornet Queen)"
+  },{
+    "id": "Two",
+    "deck": "phoenix",
+    "desc": "dont get bursted or blood mooned"
+  }];
   constructor() { }
 
   ngOnInit() {
